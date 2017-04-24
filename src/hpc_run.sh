8d89e207a0d2af60af4895ba4f7f2642a3186e1e
xue-feng <xue.feng@berkeley.edu>
2017-04-24 16:44:57 -0700

    setting hpc readable
    added Dave’s example of hpc_run.sh file and hillslope_calibrate.py

---
+++ src/hpc_run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=sa_sampling
+#
+# Account:
+#SBATCH --account=fc_drought
+#
+# Partition:
+#SBATCH --partition=savio2
+#
+# Wall clock limit:
+#SBATCH --time=1:00:00
+#
+## Command(s) to run:
+git checkout master
+x=$(python synthetic_sims.py)
+DATE=`date +%Y-%m-%d:%H:%M:%S`
+TITLESTR=“SOBOL_$DATE"
+sendmail xue.feng@berkeley.edu << EOF
+subject:$TITLESTR
+$x
+EOF
--- src/synthetic_sims.py
+++ src/synthetic_sims.py
@@ -8,7 +8,27 @@ import cPickle as pickle
 import params_constants
 from scipy import stats
 import time
+import sys
 
+# set soil conditions 
+soil_type = 'loamy_sand'
+smin = soil_dict[soil_type]['sh']
+sfc = soil_dict[soil_type]['sfc']
+sst = soil_dict[soil_type]['sst']
+n = soil_dict[soil_type]['n']
+Ks = soil_dict[soil_type]['Ksat']
+Ps = soil_dict[soil_type]['Ps']
+s = np.hstack((np.linspace(smin,sst,500), np.linspace(sst+0.001,1.0,100)))
+traits = import_traits_data()
+
+n_trajectories = 500; dt = 0.1
+lam=0.15; alpha=0.010; s1 = sfc; s0 = 0.5; Amax = 1.0/dt; Rmax = 0.10*Amax; plc = 0.5
+
+''' prep for Sobol sensitivity analysis '''
+var_names = np.array(['A_canopy','Gs_leaf','c_leaf','L_stem','A_stem','Ksat_stem','a_stem','P50_stem','L_root','A_root','Rmax'])
+n_vars = len(var_names)
+tmax=240.0; VPD = 2.0
+    
 def get_psCrit(ps, sCrit):
     return len(ps[ps<sCrit])/float(np.shape(ps)[0]*np.shape(ps)[1])
 
@@ -55,86 +75,71 @@ def generate_samples(sp, n_runs, VPD=2.0, tmax=180):
     Y_pooled = pool.map(evaluate_model, param_augmented)
     Y = np.vstack(Y_pooled) # output shape (len(param_values), 2)
     
-    with open('/Users/xuefeng/Dropbox/Projects/Isohydricity/Sobol/Si_'+sp+'_vpd'+str(int(VPD))+'_tmax'+str(tmax)+'_outcomes.pickle', 'wb') as handle:
+    with open('./Si_'+sp+'_vpd'+str(int(VPD))+'_tmax'+str(tmax)+'_outcomes.pickle', 'wb') as handle:
         pickle.dump(Y, handle)
-    with open('/Users/xuefeng/Dropbox/Projects/Isohydricity/Sobol/Si_'+sp+'_vpd'+str(int(VPD))+'_tmax'+str(tmax)+'_params.pickle', 'wb') as handle:
+    with open('./Si_'+sp+'_vpd'+str(int(VPD))+'_tmax'+str(tmax)+'_params.pickle', 'wb') as handle:
         pickle.dump(param_values, handle)
     return Y, param_values
 
-# set soil conditions 
-soil_type = 'loamy_sand'
-smin = soil_dict[soil_type]['sh']
-sfc = soil_dict[soil_type]['sfc']
-sst = soil_dict[soil_type]['sst']
-n = soil_dict[soil_type]['n']
-Ks = soil_dict[soil_type]['Ksat']
-Ps = soil_dict[soil_type]['Ps']
-s = np.hstack((np.linspace(smin,sst,500), np.linspace(sst+0.001,1.0,100)))
-traits = import_traits_data()
-
-n_trajectories = 500; dt = 0.1
-lam=0.15; alpha=0.010; s1 = sfc; s0 = 0.5; Amax = 1.0/dt; Rmax = 0.10*Amax; plc = 0.5
-
-''' prep for Sobol sensitivity analysis '''
-var_names = np.array(['A_canopy','Gs_leaf','c_leaf','L_stem','A_stem','Ksat_stem','a_stem','P50_stem','L_root','A_root','Rmax'])
-n_vars = len(var_names)
-tmax=240.0; VPD = 2.0
-
-# ''' generate samples '''
-# t0=time.time()
-# Y_pine, param_pine = generate_samples('JUNI',n_runs=100, VPD=VPD,tmax=tmax)
-# t1=time.time(); print (t1-t0)/60.0
-
-'''open samples'''
-sp = 'JUNI'
-with open('/Users/xuefeng/Dropbox/Projects/Isohydricity/Sobol/Si_'+sp+'_vpd'+str(int(VPD))+'_tmax'+str(tmax)+'_outcomes.pickle', 'rb') as handle:
-    Y = pickle.load(handle)
-with open('/Users/xuefeng/Dropbox/Projects/Isohydricity/Sobol/Si_'+sp+'_vpd'+str(int(VPD))+'_tmax'+str(tmax)+'_params.pickle', 'rb') as handle:
-    params = pickle.load(handle)
-var_dict = {'A_canopy':0,'Gs_leaf':1,'c_leaf':2,'L_stem':3,'A_stem':4,'Ksat_stem':5,'a_stem':6,'P50_stem':7,'L_root':8,'A_root':9,'Rmax':10}
-
-beta = (-2.30259/params[:, var_dict['c_leaf']])/params[:, var_dict['P50_stem']] # convert from c_leaf to P10_leaf
-delta = Ps/params[:, var_dict['P50_stem']]
-Sd, rhoH2O, Mw = params_constants.Sd, params_constants.rhoH2O, params_constants.Mw
-nu, rhoH2O, g = params_constants.nu, params_constants.rhoH2O, params_constants.g
-Sd, rhoH2O = params_constants.Sd, params_constants.rhoH2O
-d_root = 0.0005
-gc_max = params[:,var_dict['Gs_leaf']]*params[:,var_dict['A_canopy']]*Mw*Sd/(rhoH2O)
-gx_max = params[:,var_dict['Ksat_stem']]*Sd*params[:,var_dict['A_stem']]/(params[:,var_dict['L_stem']]*rhoH2O)
-gs_max = nu*Ks/(rhoH2O*g)*np.sqrt(params[:,var_dict['A_root']]/(d_root*params[:,var_dict['L_root']]))
-kappa = gs_max/gx_max; 
-f = gc_max*VPD/(gx_max*params[:, var_dict['P50_stem']])
-HR = Y[:,0]
-Assm = Y[:,1]
-print 'pause'
-
-beta_norm = beta/(max(beta)-min(beta))
-delta_norm = delta/(max(delta)-min(delta))
-kappa_norm = kappa/(max(kappa)-min(kappa))
-f_norm = f/(max(f)-min(f))
-plt.scatter(beta_norm, delta_norm, c=HR, s=50*HR+10, alpha=0.5, lw=0)
-# plt.figure()
-# plt.scatter(kappa_norm, f_norm, c=HR, s=50*HR+10, alpha=0.5, lw=0)
-plt.show()
-
-plt.plot(HR, Assm, 'o'); plt.show()
-
-''' plotting sample results '''
-fig = plt.figure(figsize=(16,4))
-out = Assm; plt.suptitle('C assimilation')
-# out = HR; plt.suptitle('Hydraulic risk')
-def plot_ind(ind, title=None): 
+def plot_ind(ind, out, title=None): 
     plt.plot(ind, out, 'o'); plt.title(title)
     slope, intercept, _, p_value, _ = stats.linregress(ind, out)
     predict_y = intercept + slope * ind
     print p_value
     if p_value<0.05: 
         plt.plot(ind, predict_y)
-ax = fig.add_subplot(141); plot_ind(beta, 'P50/Pg12') 
-ax = fig.add_subplot(142); plot_ind(delta, 'Psat_soil/P50')
-ax = fig.add_subplot(143); plot_ind(kappa, 'gmax_stem/gmax_root')
-ax = fig.add_subplot(144); plot_ind(f, '(gmax_stem*P50)/gmax_canopy*VPD')
-plt.tight_layout()
-plt.show()
+
+def main():
+    sp = 'JUNI'
+    
+    ''' generate samples '''
+    t0=time.time()
+    Y, params = generate_samples(sp,n_runs=100, VPD=VPD,tmax=tmax)
+    t1=time.time(); print (t1-t0)/60.0
+    
+#     '''open samples'''
+#     sp = 'JUNI'
+#     with open('./Si_'+sp+'_vpd'+str(int(VPD))+'_tmax'+str(tmax)+'_outcomes.pickle', 'rb') as handle:
+#         Y = pickle.load(handle)
+#     with open('./Si_'+sp+'_vpd'+str(int(VPD))+'_tmax'+str(tmax)+'_params.pickle', 'rb') as handle:
+#         params = pickle.load(handle)
+    var_dict = {'A_canopy':0,'Gs_leaf':1,'c_leaf':2,'L_stem':3,'A_stem':4,'Ksat_stem':5,'a_stem':6,'P50_stem':7,'L_root':8,'A_root':9,'Rmax':10}
+     
+    beta = (-2.30259/params[:, var_dict['c_leaf']])/params[:, var_dict['P50_stem']] # convert from c_leaf to P10_leaf
+    delta = Ps/params[:, var_dict['P50_stem']]
+    Sd, rhoH2O, Mw = params_constants.Sd, params_constants.rhoH2O, params_constants.Mw
+    nu, rhoH2O, g = params_constants.nu, params_constants.rhoH2O, params_constants.g
+    Sd, rhoH2O = params_constants.Sd, params_constants.rhoH2O
+    d_root = 0.0005
+    gc_max = params[:,var_dict['Gs_leaf']]*params[:,var_dict['A_canopy']]*Mw*Sd/(rhoH2O)
+    gx_max = params[:,var_dict['Ksat_stem']]*Sd*params[:,var_dict['A_stem']]/(params[:,var_dict['L_stem']]*rhoH2O)
+    gs_max = nu*Ks/(rhoH2O*g)*np.sqrt(params[:,var_dict['A_root']]/(d_root*params[:,var_dict['L_root']]))
+    kappa = gs_max/gx_max; 
+    f = gc_max*VPD/(gx_max*params[:, var_dict['P50_stem']])
+    HR = Y[:,0]
+    Assm = Y[:,1]
+#     
+#     beta_norm = beta/(max(beta)-min(beta))
+#     delta_norm = delta/(max(delta)-min(delta))
+#     kappa_norm = kappa/(max(kappa)-min(kappa))
+#     f_norm = f/(max(f)-min(f))
+#     plt.scatter(beta_norm, delta_norm, c=HR, s=50*HR+10, alpha=0.5, lw=0)
+#     # plt.figure()
+#     # plt.scatter(kappa_norm, f_norm, c=HR, s=50*HR+10, alpha=0.5, lw=0)
+#     plt.show()
+    
+    ''' plotting sample results '''
+    fig = plt.figure(figsize=(16,4))
+    out = Assm; plt.suptitle('C assimilation')
+    # out = HR; plt.suptitle('Hydraulic risk')
+    ax = fig.add_subplot(141); plot_ind(beta, out, 'P50/Pg12') 
+    ax = fig.add_subplot(142); plot_ind(delta, out, 'Psat_soil/P50')
+    ax = fig.add_subplot(143); plot_ind(kappa, out, 'gmax_stem/gmax_root')
+    ax = fig.add_subplot(144); plot_ind(f, out, '(gmax_stem*P50)/gmax_canopy*VPD')
+    plt.tight_layout()
+    plt.savefig('./Si_'+sp+'_vpd'+str(int(VPD))+'_tmax'+str(tmax)+'_Assm.pdf')
+    
+if __name__ == '__main__':
+    main()
 
 
