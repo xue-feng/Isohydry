d534beb1fb7545c8e47d24705c70fc922323b208
xue-feng <xue.feng@berkeley.edu>
2017-04-24 23:49:02 -0700

    editing sh file

--- src/hpc_run.sh
+++ src/hpc_run.sh
@@ -14,8 +14,8 @@
 ## Command(s) to run:
 git checkout master
 x=$(python synthetic_sims.py)
-DATE=`date +%Y-%m-%d:%H:%M:%S`
-TITLESTR=“SOBOL_$DATE"
+DATE='date +%Y-%m-%d:%H:%M:%S'
+TITLESTR='SOBOL_$DATE'
 sendmail xue.feng@berkeley.edu << EOF
 subject:$TITLESTR
 EOF
