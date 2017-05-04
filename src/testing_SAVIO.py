c7742031c222604b1339936e91791d70e835338d
xue-feng <xue.feng@berkeley.edu>
2017-05-03 18:09:09 -0700

    testing imports

--- src/testing_SAVIO.py
+++ src/testing_SAVIO.py
@@ -1,7 +1,11 @@
 import sys
+from SALib.sample import saltelli
+from scipy import stats
+import numpy as np
 
 def test():
-    sys.stdout.write('testing SAVIO!!')
+    m = np.mean(np.arange(10))
+    sys.stdout.write('testing SAVIO!! %0.2f' % (m))
     
 if __name__ == '__main__':
     test()
