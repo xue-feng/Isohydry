7569605990b495d65e5b4de6dde50d7b7c86c6d4
xue-feng <xue.feng@berkeley.edu>
2017-05-03 18:13:03 -0700

    testing imports

--- src/testing_SAVIO.py
+++ src/testing_SAVIO.py
@@ -2,10 +2,13 @@ import sys
 from SALib.sample import saltelli
 from scipy import stats
 import numpy as np
+from params_soil import soil_dict
 
 def test():
     m = np.mean(np.arange(10))
-    sys.stdout.write('testing SAVIO!! %0.2f' % (m))
+    soil_type = 'loamy_sand'
+    Ps = soil_dict[soil_type]['Ps']
+    sys.stdout.write('testing SAVIO!! The mean of array is %0.2f, Psat_soil is %0.2f' % (m, Ps))
     
 if __name__ == '__main__':
     test()
