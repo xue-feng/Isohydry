73d3032053d9035027f22e095d000179faf8b621
xue-feng <xue.feng@berkeley.edu>
2017-06-12 17:36:13 -0700

    m2m2 tmax60

--- src/testing_SAVIO.py
+++ src/testing_SAVIO.py
@@ -31,7 +31,7 @@ lam=0.05; alpha=0.010; s1 = sfc; s0 = (sfc+sst)/2.0; Amax = 1.0/dt; Rmax = 0.10*
 ''' prep for sensitivity analysis '''
 var_names = np.array(['A_canopy','Gs_leaf','c_leaf','L_stem','A_stem','Ksat_stem','a_stem','P50_stem','L_root','A_root','Rmax'])
 n_vars = len(var_names)
-VPD = 2.0; n_runs=1000; tmax=180
+VPD = 2.0; n_runs=1000; tmax=60
     
 def get_psCrit(ps, sCrit):
     return len(ps[ps<sCrit])/float(np.shape(ps)[0]*np.shape(ps)[1])
@@ -57,6 +57,9 @@ def generate_samples(sp, n_runs, VPD=2.0, tmax=180):
     problem=define_problem(sp)
     # generate samples
     param_values = saltelli.sample(problem, n_runs, calc_second_order=False); print len(param_values)
+    
+    ''' for debuggin '''
+    evaluate_model((param_values, VPD, tmax))
     
     ''' parallel processing '''
     ncores = mp.cpu_count(); print('There are %s cores on this machine '%(str(ncores),))
