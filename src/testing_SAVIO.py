5b20fbe41684e4d0eff7da734ef6dce318500bd3
xue-feng <xue.feng@berkeley.edu>
2017-06-12 22:04:47 -0700

    shorter

--- src/testing_SAVIO.py
+++ src/testing_SAVIO.py
@@ -94,7 +94,7 @@ def sample_main(sp='JUNI', tmax=180):
 if __name__ == '__main__':
     sample_main('JUNI', tmax=60)
     sample_main('PINE', tmax=60)
-    sample_main('JUNI', tmax=150)
-    sample_main('PINE', tmax=150)
+#     sample_main('JUNI', tmax=150)
+#     sample_main('PINE', tmax=150)
 
     
